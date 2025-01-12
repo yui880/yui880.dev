46dd1d4379ddcd4c7d13b9dab30d4ecb2533322a
yui880 <kimyuna521@gmail.com>
2025-01-12 20:26:34 +0900

    feat: add post header

---
+++ src/components/blog/post-header.tsx
@@ -0,0 +1,15 @@
+import { Post } from '@/config/types';
+
+const PostHeader = ({ post }: { post: Post }) => {
+  const { category, title, path, coverImage, slug, date } = post;
+  
+  return (
+    <div className="flex flex-col gap-2">
+      <p className="mb-1 w-fit rounded-md border px-2 py-0.5 text-[15px] font-medium text-code">{category}</p>
+      <p className="text-3xl font-semibold">{title}</p>
+      <p className="text-sm text-gray-500">{date}</p>
+    </div>
+  );
+};
+
+export default PostHeader;
