bfd1bfe6ca6693134bcfb7ba07f0221015ab8689
yui880 <kimyuna521@gmail.com>
2025-02-08 23:36:04 +0900

    feat: update post item layout

--- src/components/blog/post-item.tsx
+++ src/components/blog/post-item.tsx
@@ -18,10 +18,10 @@ const PostItem = ({ post }: { post: Post }) => {
           className="h-[170px] w-full max-w-full rounded-md"
           style={{ objectFit: 'cover' }}
         />
-        <div className="flex flex-col justify-center gap-1">
-          <p className="w-fit rounded-sm bg-gray-100 px-2 py-1 text-[13px] font-medium">{category}</p>
+        <div className="flex flex-col justify-center gap-1.5">
+          <p className="w-fit rounded-sm bg-gray-100 px-2 py-1 text-[12px] font-medium">{category}</p>
           <p className="text-[17px] font-medium">{title}</p>
-          <p className="text-sm text-gray-500">{date.toString()}</p>
+          <p className="text-[13px] text-gray-500">{date.toString()}</p>
         </div>
       </div>
     </Link>
